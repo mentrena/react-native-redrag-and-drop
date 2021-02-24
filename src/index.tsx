e8a45def34b044842533b686c018eff5c8b6d289
Manuel <manuel@mentrena.com>
2021-02-24 11:27:25 +0000

    docs: updating readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
-};
-
 export * from './DragDropContainer';
 export * from './DragView';
 export * from './DropView';
